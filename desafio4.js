a92fdc6a9a08ae9ffef2eed63d8323ddcf2ae1ea
Tito Perez <evisintinmarquez@escuelasproa.edu.ar>
2022-08-09 09:11:19 -0300

    naanananan q fasil

--- desafio4.js
+++ desafio4.js
@@ -2,22 +2,30 @@ function burbuja(a){
     let num = 1;
     let i = 0;
     let j = i+1;
-    let int = a.length+1;
+    let int = a.length-1;
     while (i < int) {
         console.log("intento == " + num)
         if (a[i] > a[j]){
             console.log("i == " + a[i])
             let o = a[i];
             a[i] = a[j];
+            console.log("j == " + a[j])
             a[j] = o;
-            console.log("i2 == " + a[j])
+            console.log("I2 == " + a[i])
+            console.log("J2 == " + a[j])
         }
         i++;
         j++;
         num++;
-        
+        if (i == int){
+            int--;
+            i = 0;
+            j = i+1;
+        }
     }
     return a;
 }
 
-console.log(burbuja([10,2,0,-1,7]));
+let p = burbuja([10,2,0,-1,7])
+console.log(p);
+//-1,0,2,7,10
